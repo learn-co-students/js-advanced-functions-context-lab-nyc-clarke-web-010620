0b720cc58de8c2a36484736f33912799949573af
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-05-07 13:35:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,73 @@
 /* Your Code Here */
 
+let createEmployeeRecord = function(employeeArray) {
+    return {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: [] 
+    }
+}
+
+let createEmployeeRecords = function(employeesArray) {
+    return employeesArray.map(function(employeeArray) {
+        return createEmployeeRecord(employeeArray) 
+    })
+}
+
+let createTimeInEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(' ') 
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour),
+        date: date
+    })
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    let [date, hour] = dateStamp.split(' ') 
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date
+    })
+    return this
+}
+
+let hoursWorkedOnDate = function(dateStamp) {
+    let inEvent = this.timeInEvents.find(function(e) {
+        return e.date === dateStamp
+    })
+    let outEvent = this.timeOutEvents.find(function(e) {
+        return e.date === dateStamp
+    })
+    return parseInt(outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(dateStamp) {
+    let hoursWorked = hoursWorkedOnDate.call(this, dateStamp)
+    let payRate = this.payPerHour
+    return parseFloat(hoursWorked * payRate)
+}
+
+let calculatePayroll = function(employeeRecords) {
+    let i = 0;
+    employeeRecords.forEach(employeeRecord => {
+        let onePayroll = allWagesFor.call(employeeRecord)
+        i += parseInt(onePayroll)
+    })
+    return i
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(e) {
+        return e.firstName === firstName
+    })
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
